8334a3c1a06461895b365dec8d13e62902c636f1
yaroslav <yaroslav.shlapak@gmail.com>
2019-04-11 13:18:13 +0300

    add attempt to solve this task

--- src/main/java/com/shlapak/yaroslav/leetcode/problem10/RegularExpressionMatching.kt
+++ src/main/java/com/shlapak/yaroslav/leetcode/problem10/RegularExpressionMatching.kt
@@ -14,7 +14,28 @@ class Solution {
             s == p -> result = true
         }
 
+        var index = 0
+        var current = p.getOrLast(index)
+        run loop@{
+
+        }
+        s.asSequence().forEach { char ->
+            if (char == current) {
+
+            } else {
+
+            }
+        }
+
         return result
 
+    }
+
+    private fun String.filterAndConvertToSet(): Set<Char> {
+        return asSequence().filter { it != '.' && it != '*' }.toSet()
+    }
+
+    private fun String.getOrLast(index: Int): Char {
+        return if (index >= length) last() else get(index)
     }
 }
