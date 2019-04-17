d8ea9d0217b461b2044a2d3937015c12446c9412
yaroslav <yaroslav.shlapak@gmail.com>
2019-04-17 14:11:42 +0300

    add accepted solution

--- src/test/java/com/shlapak/yaroslav/leetcode/problem15/SolutionTest.kt
+++ src/test/java/com/shlapak/yaroslav/leetcode/problem15/SolutionTest.kt
@@ -3,6 +3,7 @@
 import com.google.gson.Gson
 import com.shlapak.yaroslav.leetcode.problem15.SolutionTest.getExpected
 import com.shlapak.yaroslav.leetcode.problem15.SolutionTest.getInput
+import com.shlapak.yaroslav.leetcode.problem15.SolutionTest.getSting
 import io.kotlintest.matchers.collections.shouldContainAll
 import org.spekframework.spek2.Spek
 import java.io.File
@@ -20,8 +21,10 @@ object SolutionTest : Spek({
     var expected = getExpected()
     expected = expected
             .asSequence()
-            .map { it -> it
-                    .map { it -> it.toInt() }}
+            .map { it ->
+                it
+                        .map { it -> it.toInt() }
+            }
             .toList()
     val inputs = mapOf(
             intArrayOf(-1, 0, 1, 2, -1, -4) to
@@ -29,21 +32,46 @@ object SolutionTest : Spek({
                             listOf(-1, 0, 1),
                             listOf(-1, -1, 2)
                     ),
-            list to expected
+            list to expected,
+            intArrayOf(1, 2, -2, -1) to
+                    emptyList(),
+            intArrayOf(0, 0, 0) to
+                    listOf(
+                            listOf(0, 0, 0)
+                    ),
+            intArrayOf(0, 0, 0, 0, 0, 1, -1) to
+                    listOf(
+                            listOf(0, 0, 0),
+                            listOf(-1, 0, 1)
+                    ),
+            intArrayOf(0, 0, 0, 0, 0) to
+                    listOf(
+                            listOf(0, 0, 0)
+                    ),
+            intArrayOf(-1, 0, 1) to
+                    listOf(
+                            listOf(-1, 0, 1)
+                    )
     )
 
     group("threeSumNaive") {
         inputs.asSequence().forEach { (input, expected) ->
-            test("input: ${input.slice(0..2)} expected: ${expected.subList(0, 2)}") {
-                Solution().threeSumNaive(input) shouldContainAll expected
+            test("input: ${input.getSting()} expected: ${expected.getSting()}") {
+                val actual = Solution().threeSumNaive(input)
+                println("actual.size: ${actual.size}")
+                println("expected.size: ${expected.size}")
+                actual shouldContainAll expected
             }
         }
     }
 
     group("threeSum") {
         inputs.asSequence().forEach { (input, expected) ->
-            test("input: ${input.slice(0..2)} expected: ${expected.subList(0, 2)}") {
-                Solution().threeSum(input) shouldContainAll  expected
+            test("input: ${input.getSting()} expected: ${expected.getSting()}") {
+                val actual = Solution().threeSum(input)
+                println("actual.size: ${actual.size}")
+                println("expected.size: ${expected.size}")
+                actual shouldContainAll expected
                 /*Solution().threeSum(input).asSequence().forEach {
                     println("[${it[0]}, ${it[1]}, ${it[2]}], ")
                 }*/
@@ -54,7 +82,7 @@ object SolutionTest : Spek({
     val path = Paths.get("").toAbsolutePath().toString() +
             "/src/test/java/com/shlapak/yaroslav/leetcode/problem15/"
 
-    fun getInput(): IntArray  {
+    fun getInput(): IntArray {
         val name = "input.json"
         val inputFilePath = path + name
         val file = File(inputFilePath)
@@ -63,12 +91,40 @@ object SolutionTest : Spek({
         return gson.fromJson<List<Int>>(s, List::class.java).toIntArray()
     }
 
-    fun getExpected(): List<List<Int>>  {
+    fun getExpected(): List<List<Int>> {
         val name = "expected.json"
         val inputFilePath = path + name
         val file = File(inputFilePath)
         val s = file.readText()
         val gson = Gson()
         return gson.fromJson<List<List<Int>>>(s, List::class.java)
+    }
+
+    fun IntArray.getSting(): String {
+        return when (val size = this.size) {
+            0 -> {
+                this.toString()
+            }
+            in 0..5 -> {
+                this.slice(0 until size).toString()
+            }
+            else -> {
+                this.slice(0 until 5).toString()
+            }
+        }
+    }
+
+    fun List<List<Int>>.getSting(): String {
+        return when (val size = this.size) {
+            0 -> {
+                this.toString()
+            }
+            in 0..5 -> {
+                this.slice(0 until size).toString()
+            }
+            else -> {
+                this.slice(0 until 5).toString()
+            }
+        }
     }
 }
