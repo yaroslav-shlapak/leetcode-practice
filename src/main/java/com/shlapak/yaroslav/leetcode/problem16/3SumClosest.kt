ef6ec74bccfa7c9f4d415f5567c6f8904a2b94d2
yaroslav <yaroslav.shlapak@gmail.com>
2019-04-19 10:57:36 +0300

    add accepted but naive solution for three sum closest

--- src/main/java/com/shlapak/yaroslav/leetcode/problem16/3SumClosest.kt
+++ src/main/java/com/shlapak/yaroslav/leetcode/problem16/3SumClosest.kt
@@ -6,6 +6,20 @@ package com.shlapak.yaroslav.leetcode.problem16
  */
 class Solution {
     fun threeSumClosest(nums: IntArray, target: Int): Int {
-        return 0
+        var lastDiff = Int.MAX_VALUE
+        var res = 0
+        for (i in 0 until nums.size) {
+            for (j in i + 1 until nums.size) {
+                for (k in j + 1 until nums.size) {
+                    val threeSum = nums[i] + nums[j] + nums[k]
+                    val diff = Math.abs(target - threeSum)
+                    if (diff < lastDiff) {
+                        lastDiff = diff
+                        res = threeSum
+                    }
+                }
+            }
+        }
+        return res
     }
 }
