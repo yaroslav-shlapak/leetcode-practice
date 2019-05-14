70de7863f7f4ea0cf0000b82a4c1f86218de336e
yaroslav <yaroslav.shlapak@gmail.com>
2019-05-14 12:13:54 +0300

    add accepted solution for Remove Element

---
+++ src/main/java/com/shlapak/yaroslav/leetcode/problem27/RemoveElement.kt
@@ -0,0 +1,22 @@
+package com.shlapak.yaroslav.leetcode.problem27
+
+/**
+ * Created on 2019/05/14.
+ * https://leetcode.com/problems/remove-element/
+ */
+class Solution {
+    fun removeElement(nums: IntArray, `val`: Int): Int {
+        if (nums.isEmpty()) {
+            return 0
+        }
+        var i = 0
+        for (j in 0 until nums.size) {
+            if (nums[j] != `val`) {
+                nums[i] = nums[j]
+                i++
+            }
+        }
+
+        return i
+    }
+}
