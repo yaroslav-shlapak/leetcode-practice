fc937f35ab5eccfa9f5357857ba527ac6c556bae
yaroslav <yaroslav.shlapak@gmail.com>
2019-05-22 15:29:46 +0300

    add template for problem 30

---
+++ src/main/java/com/shlapak/yaroslav/leetcode/problem30/SubstringWithConcatenationOfAllWords.kt
@@ -0,0 +1,11 @@
+package com.shlapak.yaroslav.leetcode.problem30
+
+/**
+ * Created on 2019/05/22.
+ * https://leetcode.com/problems/substring-with-concatenation-of-all-words/
+ */
+class Solution {
+    fun findSubstring(s: String, words: Array<String>): List<Int> {
+
+    }
+}
