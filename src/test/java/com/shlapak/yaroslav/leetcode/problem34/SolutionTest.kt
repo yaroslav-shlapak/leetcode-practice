51802972df6fd4caceb1c325abe9b47686ef06df
yaroslav <yaroslav.shlapak@gmail.com>
2019-05-31 17:30:02 +0300

    add accepted solution for Find First and Last Position of Element in Sorted Array

--- src/test/java/com/shlapak/yaroslav/leetcode/problem34/SolutionTest.kt
+++ src/test/java/com/shlapak/yaroslav/leetcode/problem34/SolutionTest.kt
@@ -20,6 +20,30 @@ object SolutionTest : Spek({
             }
         }
     }
+
+    group("search2") {
+        val inputs = getInputs()
+        inputs.asSequence().forEach { (input, expected) ->
+            test("input.nums: ${input.nums.getSting()}, input.target: ${input.target}, expected: ${expected.getSting()}") {
+                val actual = Solution2().searchRange(input.nums, input.target)
+                println("actual:   ${actual.getSting()}")
+                println("expected: ${expected.getSting()}")
+                actual shouldBe expected
+            }
+        }
+    }
+
+    group("search3") {
+        val inputs = getInputs()
+        inputs.asSequence().forEach { (input, expected) ->
+            test("input.nums: ${input.nums.getSting()}, input.target: ${input.target}, expected: ${expected.getSting()}") {
+                val actual = Solution3().searchRange(input.nums, input.target)
+                println("actual:   ${actual.getSting()}")
+                println("expected: ${expected.getSting()}")
+                actual shouldBe expected
+            }
+        }
+    }
 })
 
 data class Parameter(
@@ -44,6 +68,12 @@ data class Parameter(
 }
 
 private fun getInputs(): Map<Parameter, IntArray> {
+    val size = 1000000
+    val number = 8
+    val longArray = IntArray(size)
+    for (i in 0 until size) {
+        longArray[i] = number
+    }
     return mapOf(
             Parameter(
                     intArrayOf(5, 7, 7, 8, 8, 10),
@@ -52,6 +82,18 @@ private fun getInputs(): Map<Parameter, IntArray> {
             Parameter(
                     intArrayOf(5, 7, 7, 8, 8, 10),
                     6
-            ) to intArrayOf(-1, -1)
+            ) to intArrayOf(-1, -1),
+            Parameter(
+                    intArrayOf(5, 7, 7, 8, 8, 8),
+                    8
+            ) to intArrayOf(3, 5),
+            Parameter(
+                    intArrayOf(8, 8, 8, 8, 8, 8),
+                    8
+            ) to intArrayOf(0, 5),
+            Parameter(
+                    longArray,
+                    number
+            ) to intArrayOf(0, size - 1)
     )
 }
