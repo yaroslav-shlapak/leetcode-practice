d5c39fc35c301c540da0765a03330e1d2c82c672
yaroslav <yaroslav.shlapak@gmail.com>
2019-06-15 21:48:47 +0300

    add test framework for LRU cache problem

---
+++ src/main/java/com/shlapak/yaroslav/leetcode/problem146/LRUCache.kt
@@ -0,0 +1,25 @@
+package com.shlapak.yaroslav.leetcode.problem146
+
+/**
+ * Created on 2019/06/15.
+ * https://leetcode.com/problems/lru-cache/
+ */
+class LRUCache(capacity: Int) {
+
+    fun get(key: Int): Int {
+
+        return  -1
+    }
+
+    fun put(key: Int, value: Int) {
+
+    }
+
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
---
+++ src/test/java/com/shlapak/yaroslav/leetcode/problem146/LRUCacheTest.kt
@@ -0,0 +1,73 @@
+package com.shlapak.yaroslav.leetcode.problem146
+
+import io.kotlintest.specs.FreeSpec
+import io.kotlintest.shouldBe
+
+class LRUCacheTest : FreeSpec({
+    "mainTest" - {
+        val inputs = getInputs()
+
+        for (input in inputs) {
+            "input: ${input}" - {
+                val lruCache: LRUCache = LRUCache(input.first)
+                input.second.asSequence().forEach { action ->
+                    "action: $action" {
+                        when (action) {
+                            is TestAction.GetAction -> {
+                                val actual = lruCache.get(action.key)
+                                println("actual:   ${actual}")
+                                println("expected: ${action.result}")
+                                actual shouldBe action.result
+                            }
+                            is TestAction.PutAction -> {
+                                lruCache.put(action.key, action.value)
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+})
+
+private sealed class TestAction {
+    data class GetAction(
+            val key: Int,
+            val result: Int
+    ) : TestAction()
+
+    data class PutAction(
+            val key: Int,
+            val value: Int
+    ) : TestAction()
+}
+/*
+LRUCache cache = new LRUCache( 2 /* capacity */ );
+
+cache.put(1, 1);
+cache.put(2, 2);
+cache.get(1);       // returns 1
+cache.put(3, 3);    // evicts key 2
+cache.get(2);       // returns -1 (not found)
+cache.put(4, 4);    // evicts key 1
+cache.get(1);       // returns -1 (not found)
+cache.get(3);       // returns 3
+cache.get(4);       // returns 4
+ */
+
+private fun getInputs(): List<Pair<Int, List<TestAction>>> {
+    return listOf(
+            2 to listOf(
+                    TestAction.PutAction(key = 1, value = 2),
+                    TestAction.PutAction(key = 2, value = 2),
+                    TestAction.GetAction(key = 2, result = 1),
+                    TestAction.PutAction(key = 3, value = 3),
+                    TestAction.GetAction(key = 2, result = -1),
+                    TestAction.PutAction(key = 4, value = 4),
+                    TestAction.GetAction(key = 1, result = -1),
+                    TestAction.GetAction(key = 3, result = 3),
+                    TestAction.GetAction(key = 4, result = 4)
+            )
+    )
+}
