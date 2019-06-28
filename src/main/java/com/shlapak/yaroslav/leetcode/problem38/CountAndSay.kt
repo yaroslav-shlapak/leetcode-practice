d74608a2378c0829624127f0fce365020dc61f89
Yaroslav Shlapak <yaroslav.shlapak@gmail.com>
2019-06-28 17:49:05 +0300

    add accepted solution for count and say

--- src/main/java/com/shlapak/yaroslav/leetcode/problem38/CountAndSay.kt
+++ src/main/java/com/shlapak/yaroslav/leetcode/problem38/CountAndSay.kt
@@ -5,7 +5,27 @@ package com.shlapak.yaroslav.leetcode.problem38
  */
 class Solution {
     fun countAndSay(n: Int): String {
+        var s = StringBuilder("1")
 
-        return ""
+        for (i in 2..n) {
+            val tmp = StringBuilder()
+            var current = s[0]
+            var count = 0
+            for (j in 0..s.length) {
+                if ((j < s.length && s[j] == current)) {
+                    count++
+                    if (j + 1 < s.length && s[j + 1] != current || j + 1 == s.length) {
+                        tmp.append(count)
+                        tmp.append(current)
+                        if (j + 1 < s.length) {
+                            count = 0
+                            current = s[j + 1]
+                        }
+                    }
+                }
+            }
+            s = tmp
+        }
+        return s.toString()
     }
 }
