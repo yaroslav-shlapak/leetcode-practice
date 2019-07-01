92547c70036b81bd2c7b3186fd380cf669856586
yaroslav <yaroslav.shlapak@gmail.com>
2019-07-01 18:08:24 +0300

    implement test framework for Combination Sum problem

---
+++ src/main/java/com/shlapak/yaroslav/leetcode/problem39/CombinationSum.kt
@@ -0,0 +1,14 @@
+package com.shlapak.yaroslav.leetcode.problem39
+
+/**
+ * Created on 2019/07/01.
+ * https://leetcode.com/problems/combination-sum/
+ */
+class Solution {
+    fun combinationSum(candidates: IntArray, target: Int): List<List<Int>> {
+        val list = mutableListOf<List<Int>>()
+
+
+        return list
+    }
+}
---
+++ src/test/java/com/shlapak/yaroslav/leetcode/problem39/SolutionTest.kt
@@ -0,0 +1,40 @@
+package com.shlapak.yaroslav.leetcode.problem39
+
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.FreeSpec
+
+/** tests for
+ * @see com.shlapak.yaroslav.leetcode.problem39.Solution
+ **/
+class SolutionTest : FreeSpec() {
+
+    init {
+        "combinationSum" - {
+            val inputs = getInputs()
+            inputs.asSequence().forEach { (input, expected) ->
+                "input: ${input}, expected: ${expected}" {
+                    val actual = Solution().combinationSum(input.first, input.second)
+                    actual shouldBe expected
+                }
+            }
+        }
+    }
+
+
+    private fun getInputs(): Map<Pair<IntArray, Int>, List<List<Int>>> {
+        return mapOf(
+                Pair(intArrayOf(2, 3, 6, 7), 7) to
+                        listOf(
+                                listOf(7),
+                                listOf(2, 2, 3)
+                        ),
+                Pair(intArrayOf(2, 3, 5), 8) to
+                        listOf(
+                                listOf(2, 2, 2, 2),
+                                listOf(2, 3, 3),
+                                listOf(3, 5)
+                        )
+        )
+    }
+
+}
