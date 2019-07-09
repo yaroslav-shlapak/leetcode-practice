7a66dd00ea8d2a00e607adf8d74c40af287c90ff
yaroslav <yaroslav.shlapak@gmail.com>
2019-07-09 16:20:21 +0300

    add accepted solutions for multiply strings problems

--- src/main/java/com/shlapak/yaroslav/leetcode/problem43/MultiplyStrings.kt
+++ src/main/java/com/shlapak/yaroslav/leetcode/problem43/MultiplyStrings.kt
@@ -6,6 +6,36 @@ package com.shlapak.yaroslav.leetcode.problem43
  */
 class Solution {
     fun multiply(num1: String, num2: String): String {
-        return (num1.toInt() * num2.toInt()).toString()
+        val res = try {
+            num1.toBigInteger().multiply(num2.toBigInteger()).toString()
+        } catch (e: Exception) {
+            ""
+        }
+        return res
     }
+}
+
+class SolutionNaive {
+    fun multiply(num1: String, num2: String): String {
+        val m = num1.length
+        val n = num2.length
+        val pos = IntArray(m + n)
+
+        for (i in m - 1 downTo 0) {
+            for (j in n - 1 downTo 0) {
+                val mul = (num1[i] - '0') * (num2[j] - '0')
+                val p1 = i + j
+                val p2 = i + j + 1
+                val sum = mul + pos[p2]
+
+                pos[p1] += sum / 10
+                pos[p2] = sum % 10
+            }
+        }
+
+        val sb = StringBuilder()
+        for (p in pos) if (!(sb.length == 0 && p == 0)) sb.append(p)
+        return if (sb.length == 0) "0" else sb.toString()
+    }
+
 }
--- src/test/java/com/shlapak/yaroslav/leetcode/problem43/SolutionTest.kt
+++ src/test/java/com/shlapak/yaroslav/leetcode/problem43/SolutionTest.kt
@@ -18,12 +18,26 @@ class SolutionTest : FreeSpec() {
                 }
             }
         }
+
+        "trap2" - {
+            val inputs = getInputs()
+            inputs.asSequence().forEach { (input, expected) ->
+                "input: ${input}, expected: ${expected}" {
+                    val actual = SolutionNaive().multiply(input.first, input.second)
+                    actual shouldBe expected
+                }
+            }
+        }
     }
 
     private fun getInputs(): Map<Pair<String, String>, String> {
         return mapOf(
                 Pair("2", "3") to "6",
-                Pair("123", "456") to "56088"
+                Pair("123", "456") to "56088",
+                Pair("6913259244", "71103343") to "491555843274052692",
+                Pair("498828660196", "840477629533") to "419254329864656431168468",
+                Pair("401716832807512840963", "167141802233061013023557397451289113296441069")
+                        to "67143675422804947379429215144664313370120390398055713625298709447"
         )
     }
 }
