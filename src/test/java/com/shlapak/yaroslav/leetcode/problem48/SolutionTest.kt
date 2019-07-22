5143b19f024f5530870cd53e06635ec6d14de917
yaroslav <yaroslav.shlapak@gmail.com>
2019-07-22 15:35:47 +0300

    add accepted solution for rotate matrix

--- src/test/java/com/shlapak/yaroslav/leetcode/problem48/SolutionTest.kt
+++ src/test/java/com/shlapak/yaroslav/leetcode/problem48/SolutionTest.kt
@@ -14,9 +14,11 @@ class SolutionTest : FreeSpec() {
             inputs.asSequence().forEach { (input, expected) ->
                 "input: ${input.toList()}, expected: ${expected.toList()}" {
                     Solution().rotate(input)
-                    println("actual: $input")
-                    println("expected: $expected")
-                    input shouldBe expected
+                    val act = input.asSequence().map { element -> element.toList() }.toList()
+                    val exp = expected.asSequence().map { element -> element.toList() }.toList()
+                    println("actual: ${act}")
+                    println("expected: ${exp}")
+                    act shouldBe exp
                 }
             }
         }
@@ -36,11 +38,13 @@ class SolutionTest : FreeSpec() {
                 arrayOf(
                         intArrayOf(5, 1, 9, 11),
                         intArrayOf(2, 4, 8, 10),
-                        intArrayOf(13, 3, 6, 7)
+                        intArrayOf(13, 3, 6, 7),
+                        intArrayOf(15, 14, 12, 16)
                 ) to arrayOf(
                         intArrayOf(15, 13, 2, 5),
                         intArrayOf(14, 3, 4, 1),
-                        intArrayOf(12, 6, 8, 9)
+                        intArrayOf(12, 6, 8, 9),
+                        intArrayOf(16, 7, 10, 11)
                 )
 
         )
