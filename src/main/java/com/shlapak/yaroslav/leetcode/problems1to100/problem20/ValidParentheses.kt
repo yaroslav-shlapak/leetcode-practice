c15216ffb8d4d3b88cc5970fb93d14ff10620ed9
yaroslav.shlapak <yaropslav.shlapak@gmail.com>
2025-02-18 00:12:55 +0100

    problem20

--- src/main/java/com/shlapak/yaroslav/leetcode/problems1to100/problem20/ValidParentheses.kt
+++ src/main/java/com/shlapak/yaroslav/leetcode/problems1to100/problem20/ValidParentheses.kt
@@ -3,9 +3,36 @@ package com.shlapak.yaroslav.leetcode.problems1to100.problem20
 import java.util.*
 
 /**
- * Created on 2019/05/03.
  * https://leetcode.com/problems/valid-parentheses/
+ *
  */
+
+class ValidParentheses {
+    /**
+     *  Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+     *  determine if the input string is valid.
+     *
+     *  An input string is valid if:
+     *
+     *  Open brackets must be closed by the same type of brackets.
+     *  Open brackets must be closed in the correct order.
+     *  Every close bracket has a corresponding open bracket of the same type.
+     */
+    fun isValid(s: String): Boolean {
+        val stack = Stack<Char>()
+        val map = mapOf('(' to ')', '{' to '}', '[' to ']')
+        s.forEach { c ->
+            if (stack.isNotEmpty() && map[stack.peek()] == c) {
+                stack.pop()
+            } else {
+                stack.push(c)
+            }
+        }
+        return stack.isEmpty()
+    }
+}
+
+
 class Solution {
 
     fun isValid(s: String): Boolean {
