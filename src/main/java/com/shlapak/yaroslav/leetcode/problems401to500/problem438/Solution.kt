eb41d0c888eff46e156ae23449804f0d0ee95d4d
yaroslav.shlapak <yaropslav.shlapak@gmail.com>
2025-03-09 23:51:42 +0100

    problem438

--- src/main/java/com/shlapak/yaroslav/leetcode/problems401to500/problem438/Solution.kt
+++ src/main/java/com/shlapak/yaroslav/leetcode/problems401to500/problem438/Solution.kt
@@ -2,6 +2,8 @@ package com.shlapak.yaroslav.leetcode.problems401to500.problem438
 
 /**
  *
+ * 438. Find All Anagrams in a String
+ *
  * https://leetcode.com/problems/find-all-anagrams-in-a-string/
  *
  *
@@ -34,11 +36,122 @@ package com.shlapak.yaroslav.leetcode.problems401to500.problem438
  * 1 <= s.length, p.length <= 3 * 104
  * s and p consist of lowercase English letters.
  */
+class FindAllAnagramsInString {
+    fun findAnagrams(s: String, p: String): List<Int> {
+        val sl = s.toList()
+        val pl = p.toList().sorted()
+        val result = mutableListOf<Int>()
+        val pLen = pl.size
+        for (i in 0..s.length - pLen) {
+            if (sl.subList(i, i + pLen).sorted() == pl) {
+                result.add(i)
+            }
+        }
+        return result
+    }
+}
+
+class FindAllAnagramsInString2 {
+
+    fun findAnagrams(s: String, p: String): List<Int> {
+        val result = mutableListOf<Int>()
+        val pLen = p.length
+        for (i in 0..s.length - pLen) {
+            if (isAnagram(s.substring(i, i + pLen), p)) {
+                result.add(i)
+            }
+        }
+        return result
+    }
+
+    private fun isAnagram(s1: String, s2: String): Boolean {
+        val c = IntArray(26)
+        for (s in s1) {
+            c[s - 'a'] += 1
+        }
+        for (s in s2) {
+            c[s - 'a'] -= 1
+        }
+        for (s in c) {
+            if (s != 0) return false
+        }
+        return true
+    }
+}
+
+class FindAllAnagramsInString3 {
+    fun findAnagrams(s: String, p: String): List<Int> {
+        val result = mutableListOf<Int>()
+        val pLen = p.length
+        val pMap = prepareMap(p)
+        for (i in 0..s.length - pLen) {
+            if (isAnagram(s.substring(i, i + pLen), pMap)) {
+                result.add(i)
+            }
+        }
+        return result
+    }
+
+    private fun isAnagram(s: String, pMap: MutableMap<Char, Int>): Boolean {
+        val sMap = prepareMap(s)
+        for (c in s) {
+            if (sMap[c] != pMap[c]) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun prepareMap(s: String): MutableMap<Char, Int> {
+        val pMap = mutableMapOf<Char, Int>()
+        for (c in s) {
+            val v = pMap[c]
+            if (v != null) {
+                pMap[c] = v + 1
+            } else {
+                pMap[c] = 1
+            }
+        }
+        return pMap
+    }
+}
+
+
+class FindAllAnagramsInString4 {
+
+    fun findAnagrams(s: String, p: String): List<Int> {
+        val result = mutableListOf<Int>()
+        val pLen = p.length
+        val pMap = IntArray(26)
+        for (c in p) {
+            pMap[c - 'a'] += 1
+        }
+        for (i in 0..s.length - pLen) {
+            if (isAnagram2(s.substring(i, i + pLen), pMap)) {
+                result.add(i)
+            }
+        }
+        return result
+    }
+
+    private fun isAnagram2(s1: String, pMap: IntArray): Boolean {
+        val c = IntArray(26)
+        for (s in s1) {
+            c[s - 'a'] += 1
+        }
+
+        for (s in s1) {
+            if (c[s - 'a'] != pMap[s - 'a']) return false
+        }
+        return true
+    }
+}
+
 class Solution {
     fun findAnagrams2(s: String, p: String): List<Int> {
         val result = mutableListOf<Int>()
 
-        for (i in 0 .. s.length - p.length) {
+        for (i in 0..s.length - p.length) {
             if (isAnagram(s.substring(i, i + p.length), p)) {
                 result.add(i)
             }
