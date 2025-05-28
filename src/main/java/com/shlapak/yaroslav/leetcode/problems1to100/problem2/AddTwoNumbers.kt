b4fdccfc6a38b3f793776e8a05ed4fc6a36ecfd7
yaroslav.shlapak <yaropslav.shlapak@gmail.com>
2025-05-29 00:20:33 +0200

    update progress

--- src/main/java/com/shlapak/yaroslav/leetcode/problems1to100/problem2/AddTwoNumbers.kt
+++ src/main/java/com/shlapak/yaroslav/leetcode/problems1to100/problem2/AddTwoNumbers.kt
@@ -15,6 +15,33 @@ import com.shlapak.yaroslav.leetcode.utils.ListNode
  * }
  */
 
+class AddTwoNumbers {
+    fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
+        var cur1 = l1
+        var cur2 = l2
+        var cur: ListNode = ListNode(0)
+        val res = cur
+        var quo = 0
+        //Input: l1 = [2,4,3], l2 = [5,6,4]
+        //Output: [7,0,8]
+        //Input:
+        // l1 = [9,9,9,9,9,9,9]
+        // l2 = [9,9,9,9]
+        //      [8,9,9,9,0,0,0,1]
+        while (cur1 != null || cur2 != null || quo != 0) {
+            val value = (cur1?.`val` ?: 0) + (cur2?.`val` ?: 0) + quo
+            val rem = value % 10
+            quo = value / 10
+            val newNode = ListNode(rem)
+            cur.next = newNode
+            cur = newNode
+            cur1 = cur1?.next
+            cur2 = cur2?.next
+        }
+        return res.next
+    }
+}
+
 class Solution {
     fun addTwoNumbers2(l1: ListNode?, l2: ListNode?): ListNode? {
 
