1d64c1f06c76dfef95ee96cf9b11614358dca95c
Yaroslav Shlapak <yaroslav.shlapak@just-eat.com>
2025-06-14 12:13:16 +0200

    update progress

---
+++ src/main/java/com/shlapak/yaroslav/leetcode/problems2201to2300/problem2233/MaxProductAfterKIncrements.kt
@@ -0,0 +1,27 @@
+package com.shlapak.yaroslav.leetcode.problems2201to2300.problem2233
+
+import java.util.PriorityQueue
+
+class MaxProductAfterKIncrements {
+    fun maximumProduct(nums: IntArray, k: Int): Int {
+        val minHeap = PriorityQueue<Int>()
+
+        for (n in nums) {
+            minHeap.offer(n)
+        }
+
+        for (i in 0 until k) {
+            val min = minHeap.poll()
+            minHeap.offer(min + 1)
+        }
+
+        var res = 1L
+        val mod = 1_000_000_007
+        for (n in minHeap) {
+            res = res * n
+            res = res % mod
+        }
+
+        return res.toInt()
+    }
+}
