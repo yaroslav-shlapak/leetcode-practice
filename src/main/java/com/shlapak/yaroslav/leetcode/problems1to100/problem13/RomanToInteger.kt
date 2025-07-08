a26a992c013412cd6db80eb431a52ef9813c8c1f
Yaroslav Shlapak <yaroslav.shlapak@just-eat.com>
2025-07-09 00:25:58 +0200

    update progress

--- src/main/java/com/shlapak/yaroslav/leetcode/problems1to100/problem13/RomanToInteger.kt
+++ src/main/java/com/shlapak/yaroslav/leetcode/problems1to100/problem13/RomanToInteger.kt
@@ -7,6 +7,60 @@ package com.shlapak.yaroslav.leetcode.problems1to100.problem13
 
 class RomanToInteger {
 
+    class BruteForce {
+        fun romanToInt(s: String): Int {
+            val map = mapOf(
+                'I' to 1,
+                'V' to 5,
+                'X' to 10,
+                'L' to 50,
+                'C' to 100,
+                'D' to 500,
+                'M' to 1000
+            )
+            var total = 0
+            var prevValue = 0
+
+            for (char in s) {
+                val currentValue = map[char] ?: 0
+                total += if (currentValue > prevValue) {
+                    currentValue - 2 * prevValue // subtract the previous value twice if it's less than the current
+                } else {
+                    currentValue
+                }
+                prevValue = currentValue
+            }
+
+            return total
+        }
+    }
+
+    class Clever {
+        fun romanToInt(s: String): Int {
+            val map = mutableMapOf<Char, Int>(
+                'I' to 1,
+                'V' to 5,
+                'X' to 10,
+                'L' to 50,
+                'C' to 100,
+                'D' to 500,
+                'M' to 1000
+            )
+            var res = 0
+            var prev = res
+            for (i in s.length - 1 downTo 0) {
+                val value = map[s[i]]!!
+                if (value < prev) {
+                    res -= value
+                } else {
+                    res += value
+                }
+                prev = value
+            }
+            return res
+        }
+    }
+
     inline val Char.romanValue: Int
         get() = when (this) {
             'I' -> 1
@@ -44,37 +98,37 @@ class RomanToInteger {
 class Solution {
     fun romanToInt(s: String): Int {
         val map = mapOf(
-                "" to 0,
-                "I" to 1,
-                "II" to 2,
-                "III" to 3,
-                "IV" to 4,
-                "V" to 5,
-                "VI" to 6,
-                "VII" to 7,
-                "VIII" to 8,
-                "IX" to 9,
-                "X" to 10,
-                "XX" to 20,
-                "XXX" to 30,
-                "XL" to 40,
-                "L" to 50,
-                "LX" to 60,
-                "LXX" to 70,
-                "LXXX" to 80,
-                "XC" to 90,
-                "C" to 100,
-                "CC" to 200,
-                "CCC" to 300,
-                "CD" to 400,
-                "D" to 500,
-                "DC" to 600,
-                "DCC" to 700,
-                "DCCC" to 800,
-                "CM" to 900,
-                "M" to 1000,
-                "MM" to 2000,
-                "MMM" to 3000
+            "" to 0,
+            "I" to 1,
+            "II" to 2,
+            "III" to 3,
+            "IV" to 4,
+            "V" to 5,
+            "VI" to 6,
+            "VII" to 7,
+            "VIII" to 8,
+            "IX" to 9,
+            "X" to 10,
+            "XX" to 20,
+            "XXX" to 30,
+            "XL" to 40,
+            "L" to 50,
+            "LX" to 60,
+            "LXX" to 70,
+            "LXXX" to 80,
+            "XC" to 90,
+            "C" to 100,
+            "CC" to 200,
+            "CCC" to 300,
+            "CD" to 400,
+            "D" to 500,
+            "DC" to 600,
+            "DCC" to 700,
+            "DCCC" to 800,
+            "CM" to 900,
+            "M" to 1000,
+            "MM" to 2000,
+            "MMM" to 3000
         )
         var n = 0
         var j = 0
