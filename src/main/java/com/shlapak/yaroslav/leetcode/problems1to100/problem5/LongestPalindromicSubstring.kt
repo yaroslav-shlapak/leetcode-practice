239ff166e54325ec32c465fefc00b20c8a92896a
Yaroslav Shlapak <yaroslav.shlapak@just-eat.com>
2025-07-14 13:36:02 +0200

    update progress

--- src/main/java/com/shlapak/yaroslav/leetcode/problems1to100/problem5/LongestPalindromicSubstring.kt
+++ src/main/java/com/shlapak/yaroslav/leetcode/problems1to100/problem5/LongestPalindromicSubstring.kt
@@ -6,6 +6,34 @@ import kotlin.test.assertEquals
  * 5. Longest Palindromic Substring
  * https://leetcode.com/problems/longest-palindromic-substring/description/
  */
+class Solution2 {
+    class TwoPointers {
+        fun longestPalindrome(s: String): String {
+            var max = 0
+            var res = "${s[0]}"
+
+            fun expand(lInit: Int, rInit: Int) {
+                var l = lInit
+                var r = rInit
+                while (l >= 0 && r < s.length && s[r] == s[l]) {
+                    if (max < r - l + 1) {
+                        res = s.substring(l, r + 1)
+                        max = r - l + 1
+                    }
+                    r++
+                    l--
+                }
+            }
+
+            for (i in s.indices) {
+                expand(i, i)
+                expand(i, i + 1)
+            }
+            return res
+        }
+    }
+}
+
 class LongestPalindromicSubstring {
     var start = 0
     var end = 0
