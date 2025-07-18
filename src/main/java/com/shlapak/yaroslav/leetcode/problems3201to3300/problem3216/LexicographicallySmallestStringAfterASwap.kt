32d825c6b4d9d1a59c99b9fee434cba7fdc30aeb
Yaroslav Shlapak <yaroslav.shlapak@just-eat.com>
2025-07-18 19:31:10 +0200

    update progress

---
+++ src/main/java/com/shlapak/yaroslav/leetcode/problems3201to3300/problem3216/LexicographicallySmallestStringAfterASwap.kt
@@ -0,0 +1,24 @@
+package com.shlapak.yaroslav.leetcode.problems3201to3300.problem3216
+
+class LexicographicallySmallestStringAfterASwap {
+    class BruteForce {
+        fun getSmallestString(s: String): String {
+            val s = s
+            var cand = StringBuilder(s)
+            val save = StringBuilder(s)
+            for (i in 0 until save.length - 1) {
+                val temp1 = cand[i].toInt()
+                val temp2 = cand[i + 1].toInt()
+                if (temp2 < temp1 && temp1 % 2 == temp2 % 2) {
+                    cand[i] = temp2.toChar()
+                    cand[i + 1] = temp1.toChar()
+                    return cand.toString()
+                }
+
+                cand = save
+            }
+
+            return save.toString()
+        }
+    }
+}
