4f660f2947ef71b2e246f8a934466dfbacda4781
yaroslav.shlapak <yaropslav.shlapak@gmail.com>
2025-11-01 23:57:06 +0100

    update progress

--- src/main/java/com/shlapak/yaroslav/leetcode/problems1to100/problem34/FindFirstAndLastPositionOfElementInSortedArray.kt
+++ src/main/java/com/shlapak/yaroslav/leetcode/problems1to100/problem34/FindFirstAndLastPositionOfElementInSortedArray.kt
@@ -4,6 +4,50 @@ package com.shlapak.yaroslav.leetcode.problems1to100.problem34
  * Created on 2019/05/31.
  * https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
  */
+
+class FindFirstAndLastPositionOfElementInSortedArray_Optimized {
+    class Solution {
+        fun searchRange(nums: IntArray, target: Int): IntArray {
+            //[5,7,7,7,7,7,7,7,7,8,8,10]
+            val res = intArrayOf(-1, -1)
+
+            res[0] = nums.search(target, false) { mid ->
+                mid - 1 >= 0 && nums[mid] == nums[mid - 1]
+            }
+            if (res[0] == -1) return res
+            res[1] = nums.search(target, true) { mid ->
+                mid + 1 < nums.size && nums[mid] == nums[mid + 1]
+            }
+
+            return res
+        }
+
+        private fun IntArray.search(target: Int, isRight: Boolean, exitCondition: (Int) -> Boolean): Int {
+            val nums = this
+            var r = nums.size - 1
+            var l = 0
+            while (r >= l) {
+                val mid = l + (r - l) / 2
+                when {
+                    nums[mid] > target -> r = mid - 1
+                    nums[mid] < target -> l = mid + 1
+                    else -> {
+                        if (exitCondition(mid)) {
+                            if (isRight) {
+                                l = mid + 1
+                            } else {
+                                r = mid - 1
+                            }
+                        } else {
+                            return mid
+                        }
+                    }
+                }
+            }
+            return - 1
+        }
+    }
+}
 class FindFirstAndLastPositionOfElementInSortedArray {
     fun searchRange(nums: IntArray, target: Int): IntArray {
         val res = intArrayOf(-1, -1)
